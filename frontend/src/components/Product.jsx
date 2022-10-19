32de248a8a7aee12fadb52d0d14dcc1314fee7ed
Gustavo <gustavoferdl282005@gmail.com>
2022-10-19 04:48:33 -0300

    Pagina Produto

---
+++ frontend/src/components/Product.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+import Header from "./Header";
+import Panel from "./productPanel";
+
+const Product = () => {
+    
+
+    return(
+        <> 
+            <Header></Header>
+            <Panel>
+            </Panel>
+            
+        </>
+    )
+    
+}
+
+export default Product 
